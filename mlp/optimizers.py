373e67327895979173eb09ee45ca38e203b81e51
EnzoaR012 <enzo.rezende@sou.inteli.edu.br>
2026-06-10 19:36:56 -0300

    feat: implementa otimizador SGD

--- mlp/optimizers.py
+++ mlp/optimizers.py
@@ -1,4 +1,19 @@
 """Otimizadores usados para atualizar os parametros da rede."""
 
-# TODO: implementar SGD com learning rate configuravel.
 
+class SGD:
+    """Atualiza os parametros usando stochastic gradient descent."""
+
+    def __init__(self, learning_rate=0.01):
+        if learning_rate <= 0:
+            raise ValueError("learning_rate deve ser maior que zero")
+
+        self.learning_rate = learning_rate
+
+    def update(self, parameters, gradients):
+        """Atualiza os parametros diretamente usando seus gradientes."""
+        if parameters.keys() != gradients.keys():
+            raise ValueError("parameters e gradients devem possuir as mesmas chaves")
+
+        for name in parameters:
+            parameters[name] -= self.learning_rate * gradients[name]
