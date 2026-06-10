826fd86f6dd425a94cf21f27ec4822c898699094
EnzoaR012 <enzo.rezende@sou.inteli.edu.br>
2026-06-10 19:17:28 -0300

    feat: implementa cross-entropy

--- mlp/losses.py
+++ mlp/losses.py
@@ -1,4 +1,28 @@
-"""Funcoes de perda e suas derivadas."""
+import numpy as np
 
-# TODO: implementar cross-entropy e sua derivada.
 
+def cross_entropy(y_true, y_pred):
+    epsilon = 1e-12
+    y_pred = np.clip(y_pred, epsilon, 1.0)
+
+    return -np.mean(np.sum(y_true * np.log(y_pred), axis=1))
+
+
+def cross_entropy_derivative(y_true, y_pred):
+    batch_size = y_true.shape[0]
+
+    return (y_pred - y_true) / batch_size
+
+if __name__ == "__main__":
+    y_true = np.array([
+        [1, 0, 0],
+        [0, 1, 0]
+    ])
+
+    y_pred = np.array([
+        [0.7, 0.2, 0.1],
+        [0.1, 0.8, 0.1]
+    ])
+
+    print(cross_entropy(y_true, y_pred))
+    print(cross_entropy_derivative(y_true, y_pred))
